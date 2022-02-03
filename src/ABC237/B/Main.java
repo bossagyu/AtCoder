646530f3fa9996dda6e9dc8125be216ca3ef3e0a
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2022-02-03 22:36:30 +0900

    aad abc 237 B

--- src/ABC237/B/Main.java
+++ src/ABC237/B/Main.java
@@ -1,4 +1,30 @@
 package ABC237.B;
 
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+
+    int nums[][] = new int[n][m];
+
+    for (int i = 0; i < n; i++ ) {
+      for (int j = 0; j < m; j++ ) {
+        nums[i][j] = sc.nextInt();
+      }
+    }
+
+    for(int j = 0; j < m; j++) {
+      for(int i = 0; i < n; i++) {
+        if(i != 0) {
+          System.out.print(" ");
+        }
+        System.out.print(nums[i][j]);
+      }
+      System.out.print("\n");
+    }
+  }
+}
